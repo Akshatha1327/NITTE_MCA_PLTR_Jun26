0288c5415a198269eab5475e806d3e1436d16fbd
Akshatha1327 <akshathaharinaik@gmail.com>
2026-06-24 15:10:54 +0530

    Added first folder and file in the CLI Project

--- Python CLI Project/main.py
+++ Python CLI Project/main.py
@@ -1,1 +1,9 @@
-print("hello MCA,Nitte")
+import mysql.connector
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="9995297607",
+    database="hostel_db"
+)
+if connection.is_connected():
+    print("Connected Successfully")
